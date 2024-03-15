98c09c2b9b60f5e7619d6225836ecbde87f21322
Davi Henrique Eizemberg <98131769+daviheizemberg@users.noreply.github.com>
2024-03-15 17:20:55 -0300

    Add files via upload

---
+++ ScreenSound/MainMusicaAlura.cs
@@ -0,0 +1,38 @@
+﻿//Banda queen = new Banda("Queen");
+
+
+//Album albumDoQueen = new Album("A night at the opera");
+
+//Musica musica1 = new Musica(queen, "Love of my life")
+//{
+//    Duracao = 213,
+//    Disponivel = true,
+//};
+
+//Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+//{
+//    Duracao = 354,
+//    Disponivel = false,
+//};
+
+//albumDoQueen.Adicionarmusica(musica1);
+//albumDoQueen.Adicionarmusica(musica2);
+//queen.AdicionarAlbum(albumDoQueen);
+//musica1.ExibirFichaTecnica();
+//musica2.ExibirFichaTecnica();
+//albumDoQueen.ExibirMusicasDoAlbum();
+//queen.ExibirDiscografia();
+
+Episodio ep1 = new(2, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Zeca");
+ep1.AdicionarConvidados("Marcelo");
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Davi");
+ep2.AdicionarConvidados("Henrique");
+
+Podcast podcast = new("Podzeca", "Zeca");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+
+podcast.ExibirDetalhes();
---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,33 @@
+﻿
+ class Musica
+{
+
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+    public string Nome { get; }
+    public int Duracao { get; set; }
+    public Banda Artista { get; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida =>
+         $"A música {Nome} pertence à banda {Artista}";
+
+public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}") ;
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano plus.");
+        }
+    }
+}   
+
+
